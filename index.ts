728af8dec792d3e5795421713f6675a820f02b74
mathieudutour <mathieu@dutour.me>
2022-05-06 19:57:15 +0200

    try again

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import bodyParser from "body-parser";
+
 import authorize from "./src/authorize";
 import token from "./src/token";
 import redirect from "./src/redirect";
@@ -10,5 +12,6 @@ server
   .get("/", (req, res) => res.send("Alive"))
   .get("/authorize", authorize)
   .get("/redirect", redirect)
+  .use(bodyParser.json())
   .post("/token", token)
   .listen(PORT, () => console.log(`Listening on ${PORT}`));

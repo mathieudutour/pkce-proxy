29584951d0680d416a229fe11f7b2f4109941bde
mathieudutour <mathieu@dutour.me>
2022-05-07 07:11:57 +0200

    specify host

--- index.ts
+++ index.ts
@@ -5,6 +5,8 @@ import token from "./src/token";
 import redirect from "./src/redirect";
 
 const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || "0.0.0.0";
+
 const server = fastify();
 
 server
@@ -15,7 +17,7 @@ server
 
 const start = async () => {
   try {
-    await server.listen(PORT);
+    await server.listen(PORT, HOST);
   } catch (err) {
     server.log.error(err);
     process.exit(1);

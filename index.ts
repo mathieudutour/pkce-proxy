41d551435a50c1b2d86fe5a41c2e0f002c0860fa
Mathieu Dutour <mathieu@dutour.me>
2024-02-23 15:21:56 +0100

    Fix title tag

--- index.ts
+++ index.ts
@@ -35,7 +35,7 @@ server
   .get("/", async (req, res) => {
     res.header("Content-Type", "text/html");
     res.send(`
-    <titl>OAuth PKCE Proxy</titl>
+    <title>OAuth PKCE Proxy</title>
     <h1>OAuth PKCE Proxy</h1>
     <p>This server is required to allow Raycast to connect to services that do not support the PKCE OAuth flow (which is the most secure one for applications). Ideally, this proxy should not be needed but until more services get up to speed with the latest security recommendations, this is the state that we are in.</p>
     <p><b>No data is stored in the server.</b> In fact, it doesn't even have a database.</p>
